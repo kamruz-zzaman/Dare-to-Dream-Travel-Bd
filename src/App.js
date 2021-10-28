30983333f2e6bda25f71bc58c2abefb106729bf8
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-10-29 02:52:10 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './Component/Pages/Header/Header';
 import Login from './Component/Pages/Login/Login';
 import Footer from './Component/Pages/Footer/Footer';
+import Gellery from './Component/Pages/Gellery/Gellery';
+import Banner from './Component/Pages/Banner/Banner';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <Header></Header>
       <Switch>
         <Route exact path='/'>
-
+          <Banner></Banner>
+          <Gellery></Gellery>
         </Route>
         <Route path='/login'>
           <Login></Login>
--- src/Component/Pages/Header/Header.js
+++ src/Component/Pages/Header/Header.js
@@ -8,16 +8,16 @@ const Header = () => {
     console.log(user);
     return (
         <>
-            <header className="text-gray-600 body-font bg-yellow-400">
+            <header className="text-gray-600 body-font bg-yellow-400 sticky top-0">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <Link to='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                         <img src={img} className='' alt="" />
                     </Link>
                     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                        <Link className="mr-5 text-black font-bold hover:bg-white px-5 py-2 rounded-md">First Link</Link>
-                        <Link className="mr-5 text-black font-bold hover:bg-white px-5 py-2 rounded-md">Second Link</Link>
-                        <Link className="mr-5 text-black font-bold hover:bg-white px-5 py-2 rounded-md">Third Link</Link>
-                        <Link className="mr-5 text-black font-bold hover:bg-white px-5 py-2 rounded-md">Fourth Link</Link>
+                        <Link to='/home' className="mr-5 text-black font-bold hover:bg-white px-5 py-2 rounded-md">Home</Link>
+                        <Link to='' className="mr-5 text-black font-bold hover:bg-white px-5 py-2 rounded-md">Plans</Link>
+                        <Link to='' className="mr-5 text-black font-bold hover:bg-white px-5 py-2 rounded-md">About</Link>
+                        <Link to='' className="mr-5 text-black font-bold hover:bg-white px-5 py-2 rounded-md">Gellary</Link>
 
                     </nav>
                     {
@@ -28,7 +28,7 @@ const Header = () => {
                                 <button onClick={logOut} className="inline-flex items-center text-black font-bold bg-white hover:bg-yellow-400 px-5 py-2 rounded-md">Logout</button>
                             </div>
                             :
-                            <Link to='/login'><button className="inline-flex items-center text-black font-bold bg-white hover:bg-yellow-400 px-5 py-2 rounded-md">Login</button></Link>
+                            <Link to='/login'><button className="inline-flex items-center text-black font-bold bg-white hover:bg-yellow-400 px-5 py-2 rounded-md border-2">Login</button></Link>
                     }
                 </div>
             </header>
